60702ca7f2f9989d8e1c40836ac186eb4c47d855
Christophe Marchal <toff63@gmail.com>
2018-03-04 20:04:10 +0000

    Implement run command and add integration test

--- mix_and_otp/kv_umbrella/apps/kv_server/test/kv_server_test.exs
+++ mix_and_otp/kv_umbrella/apps/kv_server/test/kv_server_test.exs
@@ -1,8 +1,34 @@
 defmodule KVServerTest do
   use ExUnit.Case
-  doctest KVServer
 
-  test "greets the world" do
-    assert KVServer.hello() == :world
+  @moduletag :capture_log
+  
+  setup do
+    Application.stop(:kv)
+    :ok = Application.start(:kv)
+  end
+
+  setup do
+    opts = [:binary, packet: :line, active: false]
+    {:ok, socket} = :gen_tcp.connect('localhost', 4040, opts)
+    %{socket: socket}
+  end
+
+  test "server interaction", %{socket: socket} do
+    assert server_client(socket, "UNKOWN COMMAND\r\n") == "UNKNOWN COMMAND\r\n"
+    assert server_client(socket, "GET SHOPPING eggs\r\n") == "NOT FOUND\r\n"
+    assert server_client(socket, "CREATE SHOPPING\r\n") == "OK\r\n"
+    assert server_client(socket, "PUT SHOPPING eggs 3\r\n") == "OK\r\n"
+    assert server_client(socket, "GET SHOPPING eggs\r\n") == "3\r\n"
+    assert server_client(socket, "") == "OK\r\n"
+    assert server_client(socket, "DELETE SHOPPING eggs\r\n") == "OK\r\n"
+    assert server_client(socket, "GET SHOPPING eggs\r\n") == "\r\n"
+    assert server_client(socket, "") == "OK\r\n"
+  end
+
+  defp server_client(socket, request) do
+    :ok = :gen_tcp.send(socket, request) 
+    {:ok, data} = :gen_tcp.recv(socket, 0, 1000)
+    data
   end
 end
