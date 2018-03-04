88d0b6a43e94c2f6ce9b4f8aefc9e6e1f00afc25
Christophe Marchal <toff63@gmail.com>
2018-03-04 16:16:37 +0000

    Add TCP server with command parsing

--- mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,67 @@
+require Logger
+
 defmodule KVServer do
-  @moduledoc """
-  Documentation for KVServer.
-  """
+  def accept(port) do
+    :observer.start()
 
-  @doc """
-  Hello world.
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
-  ## Examples
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello
-      :world
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
 
-  """
-  def hello do
-    :world
+  require IEx
+
+  defp serve(socket) do
+    msg = parse_and_execute_command(socket)
+    write_line(socket, msg)
+    serve(socket)
+  end
+
+  defp parse_and_execute_command(socket) do
+    case read_line(socket) do
+      {:ok, data} ->
+        case KVServer.Command.parse(data) do
+          {:ok, command} ->
+            KVServer.Command.run(command)
+
+          {:error, _} = err ->
+            err
+        end
+
+      {:error, _} = err ->
+        err
+    end
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, {:ok, text}) do
+    :gen_tcp.send(socket, text)
+  end
+
+  defp write_line(socket, {:error, :unknown_command}) do
+    :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
+  end
+
+  defp write_line(socket, {:error, error}) do
+    # Unknown error. Write to the client and exit.
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
+  end
+
+  defp write_line(_socket, :closed) do
+    # The connection was closed, exit politely.
+    exit(:shutdown)
   end
 end
--- mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -7,9 +7,14 @@ defmodule KVServer.Application do
 
   def start(_type, _args) do
     # List all child processes to be supervised
+    port =
+      String.to_integer(System.get_env("PORT") || raise("missing $PORT environment variable"))
+
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg)
       # {KVServer.Worker, arg},
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
