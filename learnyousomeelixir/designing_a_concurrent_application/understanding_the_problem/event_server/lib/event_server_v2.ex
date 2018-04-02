5cd0b90613c1715df532e0fa393bf6ec9edbb288
Christophe Marchal <toff63@gmail.com>
2018-04-02 18:53:50 +0000

    Change namespace and use serialized version of date time

--- learnyousomeelixir/designing_a_concurrent_application/understanding_the_problem/event_server/lib/event_server_v2.ex
+++ learnyousomeelixir/designing_a_concurrent_application/understanding_the_problem/event_server/lib/event_server_v2.ex
@@ -1,35 +1,36 @@
-defmodule Event.ServerV2 do
+defmodule Reminder.EventServerV2 do
   defmodule State do
     defstruct server: "", name: "", to_go: [0]
   end
 
   @doc """
-  iex(1)> Event.ServerV2.start(self(), "Event", NaiveDateTime.utc_now())
+  iex(1)> Reminder.EventServerV2.start(self(), "Event", NaiveDateTime.utc_now())
   #PID<0.105.0>
   iex(2)> flush()
   {:done, "Event"}
   :ok
-  iex(3)> pid = Event.ServerV2.start(self(), "Event", ~N[2200-01-01 00:00:00])
+  iex(3)> pid = Reminder.EventServerV2.start(self(), "Event", ~N[2200-01-01 00:00:00])
   #PID<0.108.0>
-  iex(4)> Event.ServerV2.cancel(pid)
+  iex(4)> Reminder.EventServerV2.cancel(pid)
   :ok
   iex(5)> flush()
   {:ok, #Reference<0.0.3.506>}
   :ok
   """
-  def start(parent, event_name, until_naive_datetime) do
-    spawn(fn -> init(parent, event_name, until_naive_datetime) end)
+  def start(parent, event_name, date_time) do
+    spawn(fn -> init(parent, event_name, date_time) end)
   end
 
-  def start_link(parent, event_name, until_naive_datetime) do
-    spawn_link(fn -> init(parent, event_name, until_naive_datetime) end)
+  def start_link(parent, event_name, date_time) do
+    spawn_link(fn -> init(parent, event_name, date_time) end)
   end
 
-  def init(server, event_name, until_naive_datetime) do
-    loop(%State{server: server, name: event_name, to_go: time_to_go(until_naive_datetime)})
+  def init(server, event_name, date_time) do
+    loop(%State{server: server, name: event_name, to_go: time_to_go(date_time)})
   end
 
-  def time_to_go(until_naive_datetime) do
+  def time_to_go({{year, month, day}, {hour,minute,second}}) do
+    until_naive_datetime = %NaiveDateTime{year: year, month: month, day: day, hour: hour, minute: minute, second: second}
     to_go = NaiveDateTime.diff(until_naive_datetime, NaiveDateTime.utc_now())
     normalize(to_go)
   end
--- learnyousomeelixir/designing_a_concurrent_application/understanding_the_problem/event_server/test/event_server_v2_test.exs
+++ learnyousomeelixir/designing_a_concurrent_application/understanding_the_problem/event_server/test/event_server_v2_test.exs
@@ -2,18 +2,18 @@ defmodule EventServerV2Test do
   use ExUnit.Case
 
   test "Normalize" do
-    assert Event.ServerV2.normalize(98 * 24 * 60 * 60) == [
+    assert Reminder.EventServerV2.normalize(98 * 24 * 60 * 60) == [
              0,
              49 * 24 * 60 * 60,
              49 * 24 * 60 * 60
            ]
 
-    assert Event.ServerV2.normalize(98 * 24 * 60 * 60 + 1) == [
+    assert Reminder.EventServerV2.normalize(98 * 24 * 60 * 60 + 1) == [
              1,
              49 * 24 * 60 * 60,
              49 * 24 * 60 * 60
            ]
 
-    assert Event.ServerV2.normalize(5) == [5]
+    assert Reminder.EventServerV2.normalize(5) == [5]
   end
 end
